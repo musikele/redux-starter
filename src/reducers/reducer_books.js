aba705f195080004f912cfa61da64f919df55b55
Michele Nasti <michele@bemind.me>
2018-01-18 18:41:07 +0100

    applicazione funzionante

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Javascript: the good parts'},
-    {title: 'Harry Potter'},
-    {title: 'The dark tower'},
-    {title: 'eloquent ruby'}
+    {title: 'Javascript: the good parts', pages: 101},
+    {title: 'Harry Potter', pages: 39},
+    {title: 'The dark tower', pages: 85},
+    {title: 'eloquent ruby', pages: 1}
   ]
 }
